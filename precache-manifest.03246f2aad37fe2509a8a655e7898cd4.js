d7c234c682a00584722a2cab3310a86b0d23b3ae
ilana <63307868+ilana009@users.noreply.github.com>
2020-05-12 21:36:14 +0100

    Added all files
    Created using CSS and React.js

---
+++ precache-manifest.03246f2aad37fe2509a8a655e7898cd4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ad754220a817c4a7c570a4af5cff95",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/my-app/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "ee53a1c9170be99687a4",
+    "url": "/my-app/static/css/main.9c77bd59.chunk.css"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/my-app/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee53a1c9170be99687a4",
+    "url": "/my-app/static/js/main.8427a1ac.chunk.js"
+  },
+  {
+    "revision": "ceff16123aa9ca3ca553",
+    "url": "/my-app/static/js/runtime-main.19c4b6ef.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/my-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
